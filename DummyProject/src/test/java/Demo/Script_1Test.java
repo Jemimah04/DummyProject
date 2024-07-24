9104300b2c2dec4bc4ed4dc32446bd80e1668a39
Jemimah04 <eilenesjemimah@gmail.com>
2024-07-24 20:35:10 +0530

    push 1 -dummy

---
+++ DummyProject/src/test/java/Demo/Script_1Test.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Script_1Test {
+	
+	@Test
+	public void scriptM1Test() {
+		System.out.println("script 1 is executed");
+	}
+
+}
---
+++ DummyProject/src/test/java/Demo/Script_2Test.java
@@ -0,0 +1,10 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Script_2Test {
+	@Test
+	public void scriptM2Test() {
+		System.out.println("script 2 is executed");
+	}
+}
