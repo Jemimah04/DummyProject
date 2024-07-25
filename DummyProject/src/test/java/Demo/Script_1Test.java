09ce8957774ca6aa056189f7f13b9f432afb50cc
Jemimah04 <eilenesjemimah@gmail.com>
2024-07-25 20:28:06 +0530

    push with poll scm

---
+++ DummyProject/src/test/java/Demo/Script_1Test.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Script_1Test {
+	
+	@Test
+	public void scriptM1Test() {
+		System.out.println("script 1 is executed");
+		//poll scm execution
+		//2nd poll
+	}
+
+}
